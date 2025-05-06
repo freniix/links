4749489635523db619a1b4dbe4a17c8d89b4da73
Himangshu <hdas9599@gmail.com>
2025-05-07 00:12:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const data =[
+    {
+        name:"sundown",
+        link:"https://freniix.github.io/sundown",
+    },
+    {
+        name:"calculator",
+        link:"https://freniix.github.io/Calculator",
+    },
+    {
+        name:"frontend mentor",
+        link:"https://freniix.github.io/frontend-mentor",
+    },
+    {
+        name:"eyes",
+        link:"https://freniix.github.io/eyes",
+    },]
+
+const wrapper = document.querySelector("#wrapper");
+data.forEach(item =>{
+    let div = document.createElement("div");
+    div.className ='links w-80 bg-gradient-to-l from-[red] to-[orange] cursor-pointer p-3 text-white  rounded-4xl text-center text-4xl';
+    let a = document.createElement('a');
+    a.href = item.link;
+    a.target = "_blank";
+    a.textContent = item.name;
+    div.appendChild(a)
+    wrapper.appendChild(div)
+});
